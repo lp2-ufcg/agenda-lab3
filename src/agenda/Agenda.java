4c7a78368fa389015a13928a38fd9b18ed12bd66
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2020-09-22 17:17:52 -0300

    Javadoc e refatoramento

--- src/agenda/Agenda.java
+++ src/agenda/Agenda.java
@@ -1,15 +1,23 @@
 package agenda;
 
+/**
+ * Um agenda que mantém uma lista de contatos com posições. Podem existir 100 contaatos 
+ * 
+ * @author nazarenoandrade
+ *
+ */
 public class Agenda {
-
+	
+	private static final int TAMANHO_AGENDA = 100;
+	
 	private String[] contatos;
 
 	public Agenda() {
-		this.contatos = new String[100];
+		this.contatos = new String[TAMANHO_AGENDA];
 	}
 	
 	public String[] getContatos() {
-		return this.contatos;
+		return this.contatos.clone();
 	}
 
 	public String getContato(int posicao) {
--- src/agenda/InterfaceAgenda.java
+++ src/agenda/InterfaceAgenda.java
@@ -5,81 +5,101 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
+/**
+ * Interface linha de comando para manipular a agenda.
+ * 
+ * @author nazarenoandrade
+ *
+ */
 public class InterfaceAgenda {
 
 	public static void main(String[] args) {
 		Agenda agenda = new Agenda();
-		
+
 		System.out.println("Carregando agenda inicial");
 		try {
+			/*
+			 * Essa é a maneira de lidar com possíveis erros por falta do arquivo. 
+			 */
 			carregaAgenda("agenda_inicial.csv", agenda);
 		} catch (IOException e) {
 			System.err.println("Erro: " + e.getMessage());
 		}
-		
+
 		Scanner scanner = new Scanner(System.in);
 		String escolha = "";
-		while(true) {			
+		while (true) {
 			escolha = menu(scanner);
 			comando(escolha, agenda, scanner);
-		} 
+		}
 
 	}
 
 	/**
 	 * Exibe o menu e captura a escolha do/a usuário/a.
 	 * 
-	 * @param scanner Para captura da opção do usuário. 
+	 * @param scanner Para captura da opção do usuário.
 	 * @return O comando escolhido.
 	 */
 	private static String menu(Scanner scanner) {
 		System.out.println(
-				"\n---\nMENU\n"
-				+ "(C)adastrar Contato\n"
-				+ "(L)istar Contatos\n"
-				+ "(E)xibir Contato\n"
-				+ "(S)air\n"
-				+ "\n"
-				+ "Opção> ");
+				"\n---\nMENU\n" + 
+						"(C)adastrar Contato\n" + 
+						"(L)istar Contatos\n" + 
+						"(E)xibir Contato\n" + 
+						"(S)air\n" + 
+						"\n" + 
+						"Opção> ");
 		return scanner.next().toUpperCase();
 	}
-	
+
+	/**
+	 * Interpreta a opção escolhida por quem está usando o sistema.
+	 * 
+	 * @param opcao   Opção digitada.
+	 * @param agenda  A agenda que estamos manipulando.
+	 * @param scanner Objeto scanner para o caso do comando precisar de mais input.
+	 */
 	private static void comando(String opcao, Agenda agenda, Scanner scanner) {
-		switch(opcao) {
-			case "c":	
-			case "C":
-				cadastraContato(agenda, scanner);
-				break;
-			case "l":
-			case "L": 
-				listaContatos(agenda);
-				break;
-			case "e":
-			case "E": 
-				exibeContato(agenda, scanner);
-				break;
-			case "s":
-			case "S": 
-				sai();
-				break;
-			default:
-				System.out.println("Opção inválida!");
+		switch (opcao) {
+		case "C":
+			cadastraContato(agenda, scanner);
+			break;
+		case "L":
+			listaContatos(agenda);
+			break;
+		case "E":
+			exibeContato(agenda, scanner);
+			break;
+		case "S":
+			sai();
+			break;
+		default:
+			System.out.println("Opção inválida!");
 		}
-		
 	}
- 
-	
+
+	/**
+	 * Imprime lista de contatos da agenda.
+	 * 
+	 * @param agenda A agenda sendo manipulada.
+	 */
 	private static void listaContatos(Agenda agenda) {
 		System.out.println("\nLista de contatos: ");
 		String[] contatos = agenda.getContatos();
-		for(int i = 0; i < contatos.length; i++) {
-			if(contatos[i] != null) {
+		for (int i = 0; i < contatos.length; i++) {
+			if (contatos[i] != null) {
 				System.out.println(formataContato(i, contatos[i]));
 			}
 		}
 	}
 
-
+	/**
+	 * Imprime os detalhes de um dos contatos da agenda. 
+	 * 
+	 * @param agenda A agenda.
+	 * @param scanner Scanner para capturar qual contato.
+	 */
 	private static void exibeContato(Agenda agenda, Scanner scanner) {
 		System.out.print("\nQual contato> ");
 		int posicao = scanner.nextInt();
@@ -87,10 +107,23 @@ public class InterfaceAgenda {
 		System.out.println("Dados do contato:\n" + formataContato(posicao, contato));
 	}
 
+	/**
+	 * Formata um contato para impressão na interface. 
+	 * 
+	 * @param posicao A posição do contato (que é exibida)/
+	 * @param contato O contato a ser impresso.
+	 * @return A String formatada.
+	 */
 	private static String formataContato(int posicao, String contato) {
 		return posicao + ": " + contato;
 	}
 
+	/**
+	 * Cadastra um contato na agenda. 
+	 * 
+	 * @param agenda A agenda.
+	 * @param scanner Scanner para pedir informações do contato.
+	 */
 	private static void cadastraContato(Agenda agenda, Scanner scanner) {
 		System.out.print("\nPosição na agenda> ");
 		int posicao = scanner.nextInt();
@@ -99,36 +132,50 @@ public class InterfaceAgenda {
 		agenda.cadastraContato(posicao, nome);
 	}
 
-
+	/**
+	 * Sai da aplicação.
+	 */
 	private static void sai() {
 		System.out.println("\nVlw flw o/");
 		System.exit(0);
 	}
 
-	
+	/**
+	 * Lê uma agenda de um arquivo csv. 
+	 * 
+	 * @param arquivo O caminho para o arquivo.
+	 * @param agenda A agenda que deve ser populada com os dados. 
+	 * @throws IOException Caso o arquivo não exista ou não possa ser lido.
+	 */
 	private static void carregaAgenda(String arquivo, Agenda agenda) throws IOException {
-		int carregados = 0; 
-		
+		int carregados = 0;
+
 		try (BufferedReader br = new BufferedReader(new FileReader(arquivo))) {
-		    String linha;
-		    while ((linha = br.readLine()) != null) {
-		    	carregados += 1;
-		    	if(carregados == 1) {
-		    		// pulamos a primeira linha, o cabeçalho
-		    		continue; 
-		    	}
-		        String[] campos = linha.split(",");
-		        processaLinhaAgendaCSV(campos, agenda);
-		    }
+			String linha;
+			while ((linha = br.readLine()) != null) {
+				carregados += 1;
+				if (carregados == 1) {
+					// pulamos a primeira linha, o cabeçalho
+					continue;
+				}
+				String[] campos = linha.split(",");
+				processaLinhaAgendaCSV(campos, agenda);
+			}
 		}
-		
+
 		System.out.println("Carregamos " + carregados + " registros.");
 	}
 
+	/**
+	 * Coloca na agenda os dados de uma linha do arquivo de agenda inicial. 
+	 * 
+	 * @param campos As informações lidas do csv. 
+	 * @param agenda A agenda a manipular. 
+	 */
 	private static void processaLinhaAgendaCSV(String[] campos, Agenda agenda) {
 		int posicao = Integer.parseInt(campos[0]);
 		String nome = campos[1].trim();
-		
+
 		agenda.cadastraContato(posicao, nome);
 	}
 }
